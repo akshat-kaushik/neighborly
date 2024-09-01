90495cddec6a2a4b5a431af609b20f63133d13c9
akshat-kaushik <93491696+akshat-kaushik@users.noreply.github.com>
2024-09-01 12:42:01 +0530

    fw

--- src/store/selectors/userEmail.ts
+++ src/store/selectors/userEmail.ts
@@ -5,6 +5,6 @@ export const userEmailState=selector({
     key:'userEmailState',
     get:({get})=>{
         const state=get(userState)
-        return state.userEmail
+        return state.userEmailn
     }
 })
